1fadea5dacbaaf8fd7e12daf3acb0bb790c605cb
JZhaoCH <1044264932@qq.com>
2018-06-01 14:49:45 +0800

    fix the error in delta_relu.m

--- activation/delta_relu.m
+++ activation/delta_relu.m
@@ -1,5 +1,6 @@
 % The derivative of relu with respect to x
 function output = delta_relu(x)
-    output = max(x,0);
-    output(output>=0) = 1;
+    output = x;
+    output(x>=0) = 1;
+    output = max(output,0);
 end
